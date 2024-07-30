002efd606c5f0757d9529cef20a4f315bfeb2042
salvatorebriga <salvo.z2302@gmail.com>
2024-07-30 18:56:26 +0200

    Milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script>
 import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
   name: "App",
+  components: {
+    ProjectCard,
+  },
+  methods: {
+    index() {
+      axios
+        .get("http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          this.projects = response.data;
+        })
+        .catch((error) => {
+          console.error("There was an error fetching the projects:", error);
+        });
+    },
+  },
   data() {
     return {
       projects: [],
     };
   },
   mounted() {
-    axios
-      .get("http://127.0.0.1:8000/api/projects")
-      .then((response) => {
-        this.projects = response.data;
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the projects:", error);
-      });
+    this.index();
   },
 };
 </script>
@@ -25,35 +34,11 @@ export default {
   <div class="container text-center">
     <h1>APP</h1>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-      <div
-        class="col mb-4 d-flex justify-content-center"
+      <ProjectCard
         v-for="project in projects"
         :key="project.id"
-      >
-        <div class="card" style="width: 18rem">
-          <img
-            src="https://www.openmindt.com/wp-content/uploads/2023/11/how-to-choose-your-programming-language-for-your-software.jpg"
-            class="card-img-top"
-            alt="Project Thumb"
-          />
-          <div class="card-body">
-            <h5 class="card-title">{{ project.name }}</h5>
-            <p class="card-text">{{ project.description }}</p>
-            <p><strong>Category:</strong> {{ project.category.name }}</p>
-            <p>
-              <strong>Technologies: </strong>
-              <span
-                v-for="(tech, index) in project.technologies"
-                :key="tech.id"
-              >
-                {{ tech.name
-                }}<span v-if="index < project.technologies.length - 1">, </span>
-              </span>
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
+        :project="project"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "ProjectCard",
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="col mb-4 d-flex justify-content-center">
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://www.openmindt.com/wp-content/uploads/2023/11/how-to-choose-your-programming-language-for-your-software.jpg"
+        class="card-img-top"
+        alt="Project Thumb"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{ project.name }}</h5>
+        <p class="card-text">{{ project.description }}</p>
+        <p><strong>Category:</strong> {{ project.category.name }}</p>
+        <p>
+          <strong>Technologies: </strong>
+          <span v-for="(tech, index) in project.technologies" :key="tech.id">
+            {{ tech.name
+            }}<span v-if="index < project.technologies.length - 1">, </span>
+          </span>
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
